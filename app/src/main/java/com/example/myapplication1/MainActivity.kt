068ae4c73724fe5af6ad2cc5f9d07ab5c6032ba2
Mahddii22 <129412250+Mahddii22@users.noreply.github.com>
2023-10-26 16:41:06 +0200

    hej

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.myapplication1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication1.ui.theme.MyApplication1Theme
 
 class MainActivity : ComponentActivity() {
@@ -28,13 +32,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
+    Surface(color = Color.Yellow) {
+        Text(
             text = "Hello $name!",
             modifier = modifier
-    )
+                .background(color = Color.Magenta)
+                .padding(16.dp)
+
+        )
+    }
 }
 
 @Preview(showBackground = true)
+@Preview(fontScale = 1.5f)
 @Composable
 fun GreetingPreview() {
     MyApplication1Theme {
